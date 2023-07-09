1e53650d234f51f4e8a4152b9cac4b4f97b72dde
Razdva94 <115868609+Razdva94@users.noreply.github.com>
2023-07-09 16:31:34 +0300

    preload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,20 +12,20 @@ import "../blocks/header/header__swiper-styles.css";
 import mareSlide8 from "../images/mareSlide8.jpg";
 import teaSlide1 from "../images/teaSlide1.jpg";
 import loresiSlide9 from "../images/loresiSlide9.jpg";
-import ImagePreloader from "./ImagePreloader";
+// import ImagePreloader from "./ImagePreloader";
 
 SwiperCore.use([Autoplay, Pagination, Navigation]);
 
 export default function Header() {
-  const imagesToPreload = [
-    MareTitle,
-    TeaTitle,
-    LoresiTitle,
-    TeaTitle,
-    mareSlide8,
-    teaSlide1,
-    loresiSlide9,
-  ];
+  // const imagesToPreload = [
+  //   MareTitle,
+  //   TeaTitle,
+  //   LoresiTitle,
+  //   TeaTitle,
+  //   mareSlide8,
+  //   teaSlide1,
+  //   loresiSlide9,
+  // ];
   const [isWideScreen, setIsWideScreen] = React.useState(false);
   const slides = [
     {
@@ -63,32 +63,32 @@ export default function Header() {
   }, []);
 
   return (
-    <ImagePreloader images={imagesToPreload}>
-      <Swiper
-        spaceBetween={0}
-        centeredSlides={true}
-        allowTouchMove={false}
-        navigation={false}
-        loop={true}
-        speed={2000}
-        slidesPerView={1}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        style={{ paddingBottom: "120px", backgroundColor: "rgb(7,7,7)" }}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        {slides.map((slide) => (
-          <SwiperSlide key={slide.id}>
-            <img
-              src={isWideScreen ? slide.image : slide.mobileImage}
-              alt={slide.alt}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </ImagePreloader>
+  // <ImagePreloader images={imagesToPreload}>
+    <Swiper
+      spaceBetween={0}
+      centeredSlides={true}
+      allowTouchMove={false}
+      navigation={false}
+      loop={true}
+      speed={2000}
+      slidesPerView={1}
+      autoplay={{
+        delay: 4000,
+        disableOnInteraction: false,
+      }}
+      style={{ paddingBottom: "120px", backgroundColor: "rgb(7,7,7)" }}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper"
+    >
+      {slides.map((slide) => (
+        <SwiperSlide key={slide.id}>
+          <img
+            src={isWideScreen ? slide.image : slide.mobileImage}
+            alt={slide.alt}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  // </ImagePreloader>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import "./pages/index.css";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store";
-import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+
+const App = lazy(() => import("./components/App"));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <Suspense fallback={<p>Loading...</p>}>
+          <App />
+        </Suspense>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
